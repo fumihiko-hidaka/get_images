8db8d518ca0f749db18b3771d6fedd5db37310cb
fumihiko_hidaka <5.p8\nABYk>
2018-09-27 00:33:15 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const { google } = require('googleapis');
+const customSearch = google.customsearch('v1');
+const mkdirp = require('mkdirp');
+const path = require('path');
+const ManyFileDownloads = require('./many_file_download');
+
+require('dotenv').config();
+
+const SEARCH_KEYWORD = process.argv[2] || '';
+const GET_ITEM_COUNT = parseInt(process.argv[3], 10) || 10;
+const CSE_ID = process.env.CSE_ID;
+const API_KEY = process.env.API_KEY;
+const SAVE_BASE_DIR = process.env.SAVE_DIR;
+
+const FILE_NAME_LENGTH = GET_ITEM_COUNT.toString().length;
+
+const saveDir = path.join(SAVE_BASE_DIR, (new Date()).toISOString());
+console.log(saveDir);
+mkdirp.sync(saveDir);
+
+const searchNum = 10;
+let startIndex = 0;
+
+const downloader = new ManyFileDownloads();
+downloader.run();
+
+const saveImages = async () => {
+
+  let itemUrls = [];
+
+  do {
+    const result = await customSearch.cse.list({
+      cx: CSE_ID,
+      q: SEARCH_KEYWORD,
+      auth: API_KEY,
+      searchType: 'image',
+      safe: 'high',
+      num: searchNum, // max:10
+      start: startIndex + 1,
+    });
+
+    itemUrls = result.data.items.map(item => item.link);
+
+    result.data.items.forEach(item => {
+      startIndex += 1;
+
+      const mimeExt = item.mime.split('/').pop() || '';
+      const linkExt = path.extname(item.link).replace('.', '').split('?').shift() || '';
+
+      const fileExt = mimeExt || linkExt || 'jpg';
+      const fileBaseName = `${'0'.repeat(FILE_NAME_LENGTH)}${startIndex}`.slice(-FILE_NAME_LENGTH);
+
+      const downloadFilePath = path.join(saveDir, `${fileBaseName}.${fileExt}`);
+
+      downloader.downloadList.push({
+        downloadPath: downloadFilePath,
+        downloadLink: item.link,
+      });
+    });
+
+    console.log(`画像URL取得中 : ${startIndex} / ${GET_ITEM_COUNT}`);
+
+  } while (startIndex < GET_ITEM_COUNT || itemUrls.length < searchNum);
+
+  return downloader.join();
+};
+
+saveImages()
+  .then(() => {
+    console.log('end')
+  })
+  .catch(e => {
+    console.error(e);
+    console.error('error');
+  });
---
+++ many_file_download.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const download = require('download');
+
+require('dotenv').config();
+
+class ManyFileDownloads {
+  constructor() {
+    this.multiDownloads = 10;
+
+    this.queue = [];
+    this.downloadList = [];
+    this.running = null;
+
+    this.sleep.bind(this);
+    this.run.bind(this);
+    this.isEnableInsertQueue.bind(this);
+    this.insertQueues.bind(this);
+    this.join.bind(this);
+  }
+
+  sleep() {
+    return new Promise(resolve => setTimeout(resolve, 1000))
+  };
+
+  run() {
+    this.running = setInterval(() => {
+      this.insertQueues();
+    }, 1000)
+  };
+
+  isEnableInsertQueue() {
+    return this.queue.length < this.multiDownloads && this.downloadList.length > 0;
+  };
+
+  insertQueues() {
+    if (!this.isEnableInsertQueue()) {
+      return;
+    }
+
+    do {
+      const { downloadPath, downloadLink } = this.downloadList.shift();
+
+      this.queue.push(downloadPath);
+
+      download(downloadLink).then(response => {
+        fs.writeFile(downloadPath, response, err => {
+          if (err) {
+            console.error(err);
+          }
+
+          console.log(`${downloadPath} -> ${downloadLink}`);
+
+          const deleteIndex = this.queue.findIndex(v => v === downloadPath);
+
+          this.queue.splice(deleteIndex, 1);
+        });
+      });
+    } while (this.isEnableInsertQueue());
+  };
+
+  async join() {
+    do {
+      await this.sleep();
+    } while (this.downloadList.length + this.queue.length > 0);
+
+    clearInterval(this.running);
+
+    return true;
+  };
+}
+
+module.exports = ManyFileDownloads;
